9133ed7c34e72bb390e8b6a563027710621f849f
Wicho <luiscalderonmiranda@gmail.com>
2022-06-01 20:39:56 -0500

    finished

--- src/components/ListaDeTareas.js
+++ src/components/ListaDeTareas.js
@@ -8,13 +8,29 @@ function ListaDeTareas() {
   const[tareas, setTareas] = useState([]);
 
   const agregarTarea = tarea => {
+    console.log(tarea)
     if (tarea.texto.trim()) {
       tarea.texto = tarea.texto.trim();
 
       const tareaActualizadas = [tarea, ...tareas];
       setTareas(tareaActualizadas);
-    }
-  }
+    };
+  };
+
+  const eliminarTarea = id => {
+    const tareasActualizadas = tareas.filter(tarea => tarea.id !== id);
+    setTareas(tareasActualizadas);
+  };
+
+  const completarTarea = id => {
+    const tareasActualizadas = tareas.map(tarea => {
+      if (tarea.id === id) {
+        tarea.completada = !tarea.completada;
+      }
+      return tarea;
+    });
+    setTareas(tareasActualizadas);
+  };
 
   return (
     <>
@@ -23,8 +39,12 @@ function ListaDeTareas() {
         {
           tareas.map((tarea) =>
             <Tarea 
+              key={tarea.id}
+              id={tarea.id}
               texto={tarea.texto}
               completada={tarea.completada}
+              completarTarea={completarTarea}
+              eliminarTarea={eliminarTarea}
             />
           )
         }
